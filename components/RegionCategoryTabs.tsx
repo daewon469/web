391ca3c415168dad2589ec9dc080bb24868fb90b
Louis <smartgauge@smartgauge.co.kr>
2026-07-11 22:04:53 +0900

    u260711-13

--- components/RegionCategoryTabs.tsx
+++ components/RegionCategoryTabs.tsx
@@ -91,7 +91,11 @@ export default function RegionCategoryTabs({ selectedRegions, onChangeRegions }:
             {SLASH_AFTER.has(label) ? (
               <div
                 aria-hidden
-                className="mx-0.5 w-px shrink-0 self-stretch bg-[#0B1B3A] sm:mx-1"
+                className="relative w-2.5 shrink-0 self-stretch sm:w-3"
+                style={{
+                  background:
+                    "linear-gradient(to bottom right, #fff 48.5%, #0B1B3A 48.5%, #0B1B3A 51.5%, #fff 51.5%)",
+                }}
               />
             ) : null}
           </Fragment>
